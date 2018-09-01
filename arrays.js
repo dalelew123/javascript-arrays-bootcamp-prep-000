fc85b1600f6a858f887268d970e9b3206f738628
Dale Lewis <dalelew123@hotmail.com>
2018-09-01 14:15:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(cities, tampa) {
-  var newCities = [tampa, cities];
-  retuen newCities;
+  retuen [tampa, ...cities]
 }
 
 
