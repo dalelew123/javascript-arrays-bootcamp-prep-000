242ef9fed465e0d711f21803de47b7dbb1e9552d
Dale Lewis <dalelew123@hotmail.com>
2018-09-01 13:53:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray() {
-  var cities = ["Tampa"];
+  var cities = [];
+  cities.push("Tampa");
 }
 
 
