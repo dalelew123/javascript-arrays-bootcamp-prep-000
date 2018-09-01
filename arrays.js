d37122ac6d5120f12527f4013555716782c3672f
Dale Lewis <dalelew123@hotmail.com>
2018-09-01 14:16:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,21 +3,3 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(cities, tampa) {
   retuen [tampa, ...cities]
 }
-
-
-function destructivelyAddElementToBeginningOfArray(cities,) {
-
-}
-
-function removeElementFromBeginningOfArray() {
-
-}
-
-function destructivelyRemoveElementFromEndOfArray () {
-
-}
-
-function removeElementFromEndOfArray(cities) {
-  var newCities = cities.pop();
-  return newCities;
-}
