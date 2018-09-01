f8658c446a090c9a5df5a3820d3b3b6e595fe1ee
Dale Lewis <dalelew123@hotmail.com>
2018-09-01 13:40:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray() {
-  
+  addElementToBeginningOfArray.push("first");
 }
-addElementToBeginningOfArray.push("first");
+
 
 var estructivelyAddElementToBeginningOfArray;
